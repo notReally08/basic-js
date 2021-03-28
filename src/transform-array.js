4b7e765d5823b21ef816ee718a6c29b880f6fcdd
notReally08 <dasha.pavlovna2014@yandex.ru>
2021-03-28 05:33:03 +0300

    refactor: make array trasform faster

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,43 +1,39 @@
 const CustomError = require("../extensions/custom-error");
-const discardNext = (arr, toDiscard) => {
-  if(toDiscard < 0 && toDiscard === arr.length - 1) return arr;
-  let newArr = [...arr];
-  newArr.splice(toDiscard, 1, null)
-  return newArr;
-};
-const discardPrev = (arr, toDiscard) => {
-  if(!toDiscard) return arr;
-  let newArr = [...arr];
-  newArr.splice(toDiscard - 1, 1, null)
-  return newArr;
-};
-const doubleNext = (arr, toDouble) => {
-  if(toDouble < 0 && toDouble === arr.length - 1) return arr;
-  let newArr = [...arr];
-  newArr.splice(toDouble, 0, arr[toDouble])
-  return newArr;
-};
-const doublePrev = (arr, toDouble) => {
-  if(!toDouble) return arr;
-  let newArr = [...arr];
-  newArr.splice(toDouble, 0, arr[toDouble - 1])
-  return newArr;
-};
-const markers = [
-  "--discard-next", 
-  "--discard-prev",
-  "--double-next",
-  "--double-prev"
-];
 
-const obj = {
-  0: discardNext,
-  1: discardPrev,
-  2: doubleNext,
-  3: doublePrev
-}
 let res = undefined;
 module.exports = function transform(arr) {
+  const discardNext = (arr, toDiscard) => {
+    if(toDiscard < 0 && toDiscard === arr.length - 1) return arr;
+    arr.splice(toDiscard, 1, null)
+    return newArr;
+  };
+  const discardPrev = (arr, toDiscard) => {
+    if(!toDiscard) return arr;
+    arr.splice(toDiscard - 1, 1, null)
+    return newArr;
+  };
+  const doubleNext = (arr, toDouble) => {
+    if(toDouble < 0 && toDouble === arr.length - 1) return arr;
+    arr.splice(toDouble, 0, arr[toDouble])
+    return newArr;
+  };
+  const doublePrev = (arr, toDouble) => {
+    if(!toDouble) return arr;
+    arr.splice(toDouble, 0, arr[toDouble - 1])
+    return newArr;
+  };
+  const markers = [
+    "--discard-next", 
+    "--discard-prev",
+    "--double-next",
+    "--double-prev"
+  ];
+  const obj = {
+    0: discardNext,
+    1: discardPrev,
+    2: doubleNext,
+    3: doublePrev
+  }
   let indexOfMarkerInArr = -1;
   let indexOfMarkerInObj = -1;
   if(!arr instanceof Array) throw new Error();
@@ -45,7 +41,7 @@ module.exports = function transform(arr) {
   let newArr = [...arr];
   let end = false;
   for(let i = 0; i < newArr.length; i++) {
-    if(markers.some(it => it === newArr[i])) {
+    if(markers.includes(newArr[i])) {
       indexOfMarkerInArr = i;
       indexOfMarkerInObj = markers.indexOf(newArr[i]);
       newArr.splice(indexOfMarkerInArr, 1)
@@ -53,8 +49,7 @@ module.exports = function transform(arr) {
     }
   }
   if(indexOfMarkerInObj >= 0) {
-    let transformHandler = obj[indexOfMarkerInObj];
-    res = transformHandler(newArr, indexOfMarkerInArr);
+    res = obj[indexOfMarkerInObj](newArr, indexOfMarkerInArr);
   } else {
     res = newArr;
     end = true 
